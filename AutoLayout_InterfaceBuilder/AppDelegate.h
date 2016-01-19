e88633953362de332a1078334e99bab941d92f8f
narendrathapa <thapa.narendra@gmail.com>
2016-01-19 14:18:28 -0500

    Initial Commit

---
+++ AutoLayout_InterfaceBuilder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout_InterfaceBuilder
+//
+//  Created by Narendra Thapa on 2016-01-19.
+//  Copyright © 2016 Narendra Thapa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout_InterfaceBuilder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout_InterfaceBuilder
+//
+//  Created by Narendra Thapa on 2016-01-19.
+//  Copyright © 2016 Narendra Thapa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
